a80102b013ee6044d63673660a6cc1ac20a6f672
gitshawn <shawn@nulogix.net>
2022-04-23 07:40:13 -0400

    Update AWSECSWrangleASG.js

--- functions/AWSECSWrangleASG.js
+++ functions/AWSECSWrangleASG.js
@@ -14,17 +14,13 @@ const drainingInstanceTimeoutSeconds = 60;
 
 const reWrangleTimeoutSeconds = 10;
 
-const awsconfig = {
-    region: "us-west-2"
-};
-
-const ecsclient = new ECSClient(awsconfig);
-
-const ASGClient = new AutoScalingClient(awsconfig);
-
 const params = {
-    cluster: "wheels-prod",
+    region: process.env.CW_VAR_1 || "us-west-2",
+    cluster: process.env.CW_VAR_2 || "engine-stg",
+    capacityprovider: process.env.CW_VAR_3 || ""
 };
+const ecsclient = new ECSClient(params);
+const asgclient = new AutoScalingClient(params);
 
 const run = async () => {
 
@@ -33,7 +29,7 @@ const run = async () => {
         const containerInstances = await ecsclient.send(
             new ListContainerInstancesCommand({
                 cluster: params.cluster,
-                status: "ACTIVE"
+                status: "ACTIVE",
             }))
         // Get the detail for those containerInstanceArns
         const describeContainerInstances = await ecsclient.send(
@@ -42,71 +38,92 @@ const run = async () => {
                 containerInstances: containerInstances.containerInstanceArns
             }))
 
-        //console.log(describeContainerInstances);
+        // If capacityprovider is specified, parse the container list for only the containers in that capacity provider
+        let containersToEvaluate = []; 
+        if (params.capacityprovider) {
+            containersToEvaluate = describeContainerInstances.containerInstances.filter(record => record.capacityProviderName == params.capacityprovider);
+        }
+        else {
+            containersToEvaluate = describeContainerInstances.containerInstances;
+        }
 
-        let instanceWithMostAvailableResourcesID;
-        let instanceWithMostAvailableResourcesAmount = 0;
-        let instanceWithMostAvailableResourcesUsedAmount = 0;
-        let totalAvailableResources = 0;
+        console.log(`Running AWSECSWrangleASG on cluster: ${params.cluster} and Capacity Provider: ${containersToEvaluate[1].capacityProviderName}`);
 
         // Calculate the container instance with the most available resources
         // Calculate total available resources
         // Calculate used resources per container instance 
-        for (const containerInstance of describeContainerInstances.containerInstances) {
-
-            //console.log(containerInstance.ec2InstanceId);
+        let instanceWithMostResources = {
+            ID: '',
+            ARN: '',
+            FreeResources: 0,
+            UsedResources: 0
+        };
+        let totalAvailableResources = 0;
+        for (const containerInstance of containersToEvaluate) {
 
            const freeResourceJSON = containerInstance.remainingResources.find(remainingResources => remainingResources.name == "MEMORY");
            const freeResource = parseInt(freeResourceJSON.integerValue);
            const usedResourceJSON = containerInstance.registeredResources.find(registeredResources => registeredResources.name == "MEMORY");
            const usedResource = parseInt(usedResourceJSON.integerValue) - freeResource;
            totalAvailableResources = totalAvailableResources + freeResource;
-           
-           //console.log(freeResource); 
-           //console.log(usedResource);
+           console.log(`Container instance: ${containerInstance.ec2InstanceId} has ${usedResource} resource(s) used and ${freeResource} resource(s) available`);
 
-           if (freeResource > instanceWithMostAvailableResourcesAmount) {
-                instanceWithMostAvailableResourcesAmount = freeResource;
-                instanceWithMostAvailableResourcesUsedAmount = usedResource;
-                instanceWithMostAvailableResourcesID = containerInstance.ec2InstanceId;
+           // If this instance has more resources available then the last highest, make this instance the highest
+           if (freeResource > instanceWithMostResources.FreeResources) {
+                instanceWithMostResources.FreeResources = freeResource;
+                instanceWithMostResources.UsedResources = usedResource;
+                instanceWithMostResources.ID = containerInstance.ec2InstanceId;
+                instanceWithMostResources.ARN = containerInstance.containerInstanceArn;
            }
-
-           //console.log(totalAvailableResources)
-
         }
 
-        // console.log(`The instance with the most amount of memory available is ${instanceWithMostAvailableResourcesAmount}`);
+        console.log(`There are ${totalAvailableResources} resource(s) available across the ASG`);
+        console.log(`The least used instance is ${instanceWithMostResources.ID}, it is using ${instanceWithMostResources.UsedResources} resource(s)`);
 
         // Calculate whether to wrangle or not
-        const freeResourcesAfterWrangle = (totalAvailableResources - instanceWithMostAvailableResourcesAmount) - (instanceWithMostAvailableResourcesUsedAmount);
+        const freeResourcesAfterWrangle = (totalAvailableResources - instanceWithMostResources.FreeResources) - (instanceWithMostResources.UsedResources);
         if ( freeResourcesAfterWrangle > 0) {
-            console.log(`We will wrangle, as we have ${freeResourcesAfterWrangle} memory available after this wrangle. This begins by draining the instance ${instanceWithMostAvailableResourcesID}`);
+            console.log(`We will wrangle, as we have ${freeResourcesAfterWrangle} resource(s) available after this wrangle. This begins by draining the instance ${instanceWithMostResources.ID}`);
             // We will wrangle, drain the instance in question
             const drainingInstance = await ecsclient.send(
                 new UpdateContainerInstancesStateCommand({
-                    containerInstances: instanceWithMostAvailableResourcesID,
+                    cluster: params.cluster,
+                    containerInstances: [].concat(instanceWithMostResources.ARN),
                     status: "DRAINING"
                 }))
+
+            // Wait until the instance has 0 running tasks     
             let drainingInstanceTimeoutCounter = 0;
-            // Wait until the instance has 0 running tasks 
+            let drainingInstanceRunningTasks = 1;
             do {
-                await new Promise(x => setTimeout(x, 1000));
-                const runningTasksJSON = containerInstance.find(ec2InstanceId => ec2InstanceId == instanceWithMostAvailableResourcesID);
-                const runningTasks = parseInt(runningTasksJSON.runningTasksCount);
+                // The division by 5 here is a throttling timeout pattern; to ensure that the ecsclient is not flooded with requests
+                await new Promise(x => setTimeout(x, (drainingInstanceTimeoutSeconds / 5) * 1000));
+                const drainingInstanceUpdate = await ecsclient.send(
+                    new DescribeContainerInstancesCommand({
+                        cluster: params.cluster,
+                        containerInstances: [].concat(instanceWithMostResources.ARN)
+                    }))
+
+                drainingInstanceRunningTasks = parseInt(drainingInstanceUpdate.containerInstances.runningTasksCount);
+
                 drainingInstanceTimeoutCounter++;
-                // If we have reached draining timeout, throw an error
-                if ( drainingInstanceTimeoutCounter = drainingInstanceTimeoutSeconds ) {
+
+                // Another part of throttling timeout pattern, 5 + 1 will trip the timeout and throw an error
+                if (drainingInstanceTimeoutCounter == 6) {
                     throw `Tried to wrangle but instance did not drain within timeout of ${drainingInstanceTimeoutSeconds} seconds`
                 }
-            } while (runningTasks != 0);
-            // Terminate the instance now it has drained
-            const terminatingInstance = await ASGClient.send(
+            } while (drainingInstanceRunningTasks >> 0);
+
+            // Terminate the instance now that it has fully drained
+            const terminatingInstance = await asgclient.send(
                 new TerminateInstanceInAutoScalingGroupCommand({
-                    InstanceId: instanceWithMostAvailableResourcesID,
+                    InstanceId: instanceWithMostResources.ID,
                     ShouldDecrementDesiredCapacity: true
                 }))
+                
             // Log the Auto Scaling Group activity that was triggered based off of the instance termination
-            console.log(terminatingInstance.Activity);
+            console.log(`Terminated instance ${instanceWithMostResources.ID}, ASG replies with: ${terminatingInstance.Activity.Cause}`);
+            
             // Sleep and run this script again, as we could have more instances to wrangle    
             await new Promise(x => setTimeout(x, (reWrangleTimeoutSeconds * 1000)));
             run();
