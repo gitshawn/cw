ff7a416b3fd4daf8900c6e6720aab3cc3b51af24
gitshawn <shawn@nulogix.net>
2022-04-23 07:50:35 -0400

    Update cw.js

--- cw.js
+++ cw.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 /**
- * This is the CLI frontend to Cloud Wrangler functions. 
+ * cw.js
+ * This is the CLI frontend to CloudWrangler functions. 
  * 
  * It:
  * - Parses the CLI command
@@ -16,18 +17,21 @@
 const run = async () => {
 
     try {
+        // Parse the CLI string
         const cliString = Array.from(process.argv);
         const args = cliString.splice(process.execArgv.length + 2);
 
-        let functionName = '';
-
+        // Check to see if there are any arguments to cw.js
         if (args == '') throw "No arguments provided. Please provide at least a function name to execute.";
 
+        // Parse the arguments to cw.js, the first one is the functionName, the others are set as env vars
+        let functionName = '';
         args.forEach((element, index) => {
             if (functionName == '') functionName = element;
             else process.env['CW_VAR_' + index] = element;
         });
     
+        // Run the function and wait
         const functionToRun = await import(`./functions/${functionName}.js`);
 
         } catch (err) {
