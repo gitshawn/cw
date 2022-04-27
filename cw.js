29f1900489ed3b243816b56659d985e1e2c37a15
Shawn Friedel <shawn@nulogix.net>
2022-04-27 11:22:24 -0400

    Update cw.js

--- cw.js
+++ cw.js
@@ -7,11 +7,11 @@
  * It:
  * - Parses the CLI command
  * - Pulls out the function name
- * - Sets the rest of the params as env vars
+ * - Sets the rest of the params as args to the function
  * - Executes the function
  * 
  * Example usage:
- * node cw AWSECSWrangleASG [clustername] 
+ * node cw AWSECSWrangleASG [region] [clustername]
  */
 
 const run = async () => {
@@ -19,21 +19,18 @@ const run = async () => {
     try {
         // Parse the CLI string
         const cliString = Array.from(process.argv);
-        const args = cliString.splice(process.execArgv.length + 2);
+        const params = cliString.splice(process.execArgv.length + 2);
 
         // Check to see if there are any arguments to cw.js
-        if (args == '') throw "No arguments provided. Please provide at least a function name to execute.";
+        if (params == '') throw "No arguments provided. Please provide at least a function name to execute.";
 
         // Parse the arguments to cw.js, the first one is the functionName, the others are set as env vars
-        let functionName = '';
-        args.forEach((element, index) => {
-            if (functionName == '') functionName = element;
-            else process.env['CW_VAR_' + index] = element;
-        });
-    
+        let functionName = params[0];
+        const args = params.slice(1);
+
         // Run the function and wait
         const functionToRun = await import(`./functions/${functionName}.js`);
-        await functionToRun.run();
+        await functionToRun.run(...args);
 
         } catch (err) {
             console.log("Error", err);
